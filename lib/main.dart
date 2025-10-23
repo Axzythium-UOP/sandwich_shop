39a26f15c9dee60c1fcb7ed2f50e31180533f4c5
Axzythium-UOP <daniel.t.gardner@hotmail.com>
2025-10-23 23:52:16 +0100

    worksheet 2 ready

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,60 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: OrderItemDisplay(5, 'Footlong'),
+        body: const SafeArea(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              height: 150,
+              child: OrderItemDisplayRow(),
+            ),
+          ),
         ),
       ),
     );
   }
 }
 
+class OrderItemDisplayRow extends StatelessWidget {
+  const OrderItemDisplayRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.blue,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          OrderItemDisplay(3, 'BLT'),
+          OrderItemDisplay(5, 'Club'),
+          OrderItemDisplay(2, 'Veggie'),
+        ],
+      ),
+    );
+  }
+}
+
 class OrderItemDisplay extends StatelessWidget {
-  final String itemType;
   final int quantity;
+  final String itemType;
 
   const OrderItemDisplay(this.quantity, this.itemType, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+    final sandwiches = List.filled(quantity, '🥪').join();
+    return Text(
+      '$quantity $itemType sandwich(es): $sandwiches',
+      style: const TextStyle(color: Colors.white, fontSize: 16),
+      textAlign: TextAlign.center,
+    );
   }
 }
